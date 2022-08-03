db8f4e637a8e9e22a1fcd37949a3db943164d823
SDenn12 <samuel.dennis0@gmail.com>
2022-08-03 10:56:21 +0100

    first commit

---
+++ control_flow.py
@@ -0,0 +1,37 @@
+# if, elif, else
+
+
+# learn loops (for/while)
+# dont have to repeat yourself
+# help with iteration
+
+shopping_list = ["fruits", "milk", "bread", "cream"]
+print(shopping_list)
+for item in shopping_list:
+    print(item)
+    if item == "bread":
+        break
+
+dictionary = {
+    'name': 'Sam',
+    'age': 21,
+    'salary': 10,
+    'address': 'AA11AA',
+    'spir_ani': 'panda',
+    'fav_col': 'red'
+}
+
+for key in dictionary.keys():
+    print(key)
+
+for value in dictionary.values():
+    print(value)
+
+for key in dictionary.keys():
+    print(f"{key} - {dictionary[key]}")
+
+# print("\n")
+# i = 0
+# while i < len(shopping_list):
+#     print(shopping_list[i])
+#     i += 1
