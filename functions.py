d101f3c6f52c06b2329ab05f778b82f002b3b3ae
SDenn12 <samuel.dennis0@gmail.com>
2022-08-03 13:49:53 +0100

    calculator added

--- functions.py
+++ functions.py
@@ -16,29 +16,35 @@ def greet_user(name):
 
 greet_user("Sam")
 
+
 # create a function that takes two arguments
 
 
 class Calculator:
+    # creates add method
+    # declares static method (unrelated to the class object itself, convenient for storing related methods)
     @staticmethod
     def add(num1, num2):
         return num1 + num2
 
+    # creates subtract method
     @staticmethod
     def subtract(num1, num2):
         return num1 - num2
 
+    # creates multiply method
     @staticmethod
     def multiply(num1, num2):
         return num1 * num2
 
+    # creates divide method
     @staticmethod
     def divide(num1, num2):
         return num1 / num2
 
 
+# prints the functions
 print(Calculator.add(1, 2))
 print(Calculator.subtract(1, 2))
 print(Calculator.multiply(1, 2))
 print(Calculator.divide(1, 2))
-
